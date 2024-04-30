c6454f826c26c982030761367f9b8326b3b6eb61
Simon <simon.lowe90@hotmail.co.uk>
2024-04-30 17:00:01 +0100

    Finished adding content working on Services section.

--- wp-content/themes/sml-web-development/assets/js/swiper-config.js
+++ wp-content/themes/sml-web-development/assets/js/swiper-config.js
@@ -1,4 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
   });
--- wp-content/themes/sml-web-development/template-parts/services.php
+++ wp-content/themes/sml-web-development/template-parts/services.php
@@ -7,6 +7,7 @@
         $ecommDescription = get_sub_field('e_comm_service_description');
         $non_ecommService = get_sub_field('non_e_comm_service_title');
         $non_ecommDescription = get_sub_field('non_e_comm_service_description');
+        $service_image = get_sub_field('service_image');
 ?>
 
 <div class="services-frame absolute top-0 xl:w-[536px] xl:h-[99.16px] lg:w-[450px] md:w-[350px] sm:w-[250px] xs:w-[250px] w-56 flex items-center justify-center">
@@ -14,51 +15,33 @@
     <h2 class="text-sp-blue"><?php echo $servicesTitle; ?></h2>
 </div>
 
-<!-- <div class="service-container flex justify-between w-full absolute top-44 container">
-
-    <div class="ecomm flex xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] justify-center items-center">
-        <img src="/wp-content/uploads/2024/04/bnspyrd_stock_postal_steampunk_ochre_png_by_bnspyrd_d4n74ya.png" alt="" class="absolute xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] opacity-80">
-        <div class="services-content flex flex-col absolute top-14 h-full xl:w-[25rem] lg:w-80">
-            <h3 class="flex justify-center items-center underline"><?php echo $ecommService; ?></h3>
-            <div class="absolute top-16">
-                <?php echo $ecommDescription; ?>
-            </div>
-        </div>
-    </div>
-
-    <div class="non-ecomm flex xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] justify-center items-center">
-        <img src="/wp-content/uploads/2024/04/bnspyrd_stock_postal_steampunk_ochre_png_by_bnspyrd_d4n74ya.png" alt="" class="absolute xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] opacity-80">
-        <div class="services-content flex flex-col absolute top-14 h-full xl:w-[25rem] lg:w-80">
-            <h3 class="flex justify-center items-center underline"><?php echo $non_ecommService; ?></h3>
-            <div class="absolute top-16">
-                <?php echo $non_ecommDescription; ?>
-            </div>
-        </div>
-    </div>
-
-</div> -->
-
 <div class="swiper mySwiper left-0 top-40 xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem]">
     <div class="swiper-wrapper">
       <div class="swiper-slide ecomm flex xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] justify-center items-center">
-      <img src="/wp-content/uploads/2024/04/bnspyrd_stock_postal_steampunk_ochre_png_by_bnspyrd_d4n74ya.png" alt="" class="absolute xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] opacity-80">
-        <div class="services-content flex flex-col absolute top-14 h-full xl:w-[25rem] lg:w-80">
+      <img src="/wp-content/uploads/2024/04/parchment-background-scaled.jpg" alt="" class="absolute xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] opacity-80">
+        <div class="services-content flex flex-col absolute translate-x-[3.125rem] top-14 h-full xl:w-[25rem] lg:w-80">
             <h3 class="flex justify-center items-center underline"><?php echo $ecommService; ?></h3>
             <div class="absolute top-16">
                 <?php echo $ecommDescription; ?>
             </div>
         </div>
       </div>
-      <div class="swiper-slide non-ecomm flex xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] justify-center items-center"><img src="/wp-content/uploads/2024/04/bnspyrd_stock_postal_steampunk_ochre_png_by_bnspyrd_d4n74ya.png" alt="" class="absolute xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] opacity-80">
-        <div class="services-content flex flex-col absolute top-14 h-full xl:w-[25rem] lg:w-80">
+      <div class="swiper-slide non-ecomm flex xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] justify-center items-center">
+        <img src="/wp-content/uploads/2024/04/parchment-background-scaled.jpg" alt="" class="absolute xl:w-[31rem] xl:h-[40rem] md:w-96 md:h-[35rem] opacity-80">
+        <div class="services-content flex flex-col absolute translate-x-[3.125rem] top-14 h-full xl:w-[25rem] lg:w-80">
             <h3 class="flex justify-center items-center underline"><?php echo $non_ecommService; ?></h3>
             <div class="absolute top-16">
                 <?php echo $non_ecommDescription; ?>
             </div>
-        </div></div>
-      <div class="swiper-slide">Slide 3</div>
+        </div>
     </div>
   </div>
+  <div class="swiper-pagination"></div>
+</div>
+
+<div class="image-container w-[31rem] h-[40rem] absolute top-40 right-20">
+  <img src="<?php echo $service_image; ?>" alt="" class="h-full w-full" >
+</div>
 
 
 

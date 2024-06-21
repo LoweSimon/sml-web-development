9aa7cc8ee774782d9e1829816055bdacc7af65bc
Simon <simon.lowe90@hotmail.co.uk>
2024-06-21 21:24:12 +0100

    Adding some more styling for hover states

--- wp-content/themes/sml-web-development-v2/theme/template-parts/about-me.php
+++ wp-content/themes/sml-web-development-v2/theme/template-parts/about-me.php
@@ -3,7 +3,7 @@
         while(have_rows('about_me')): the_row();
 
         $aboutHeader = get_sub_field('about_me_header');
-        $aboutText = get_sub_field('about_me_text', false, false);
+        $aboutText = get_sub_field('about_me_text');
         $aboutImage = get_sub_field('about_me_image');
 ?>
 
@@ -11,7 +11,9 @@
     <div class="container flex lg:flex-row flex-col lg:justify-between items-center py-16">
         <div class="about-content lg:w-1/2 w-full">
             <h2 class="text-2xl font-bold underline pb-10"><?php echo $aboutHeader ?></h2>
-            <p><?php echo $aboutText ?></p>
+            <div class="">
+                <?php echo $aboutText ?>
+            </div>
         </div>
         <div class="about-image-container max-w-96 w-full h-auto lg:pt-0 pt-5">
             <img src="<?php echo $aboutImage ?>" alt="" class="w-full h-full">
--- wp-content/themes/sml-web-development-v2/theme/template-parts/hero.php
+++ wp-content/themes/sml-web-development-v2/theme/template-parts/hero.php
@@ -2,29 +2,35 @@
     if(have_rows('hero_section')):
         while(have_rows('hero_section')): the_row();
 
+        $heroOption = get_sub_field('hero_option');
         $heroVideo = get_sub_field('hero_video');
         $heroImage = get_sub_field('hero_image');
         $heroHeader = get_sub_field('hero_header');
         $heroHeaderSize = get_sub_field('hero_header_size');
         $heroHeaderTwo = get_sub_field('hero_header_two');
         $heroHeaderSizeTwo = get_sub_field('hero_header_size_two');
-        $heroText = get_sub_field('hero_text', false, false);
+        $heroText = get_sub_field('hero_text');
         $heroButton = get_sub_field('hero_cta');
 ?>
 
 <section class="hero w-screen max-h-[38rem] h-full absolute top-0 z-20">
 
-        <video autoplay loop muted class="hero_image w-full h-full relative flex object-fill">
-            <source src="<?php echo $heroVideo; ?>" />
-        </video>
-        <!-- <div class="hero_image h-full relative" style="background: url(<?php echo $heroImage ?>) lightgray 50% / cover no-repeat;"></div> -->
+        <?php if($heroOption == 'video'): ?>
+            <video autoplay loop muted class="hero_image w-full h-full relative flex object-fill">
+                <source src="<?php echo $heroVideo; ?>" />
+            </video>
+        <? else : ?>
+            <div class="hero_image h-full relative" style="background: url(<?php echo $heroImage ?>) lightgray 50% / cover no-repeat;"></div>
+        <?php endif; ?>
 
         <div class="hero-content container relative w-full h-auto top-[-27rem]">
             <div class="max-w-3xl lg:w-full max-h-80 lg:h-full bg-header-box rounded-3xl">
                 <div class="px-9 py-5">
                     <<?php echo $heroHeaderSize ?> class="text-3xl text-white font-normal"><?php echo $heroHeader ?></<?php echo $heroHeaderSize ?>>
                     <<?php echo $heroHeaderSizeTwo ?> class="text-4xl text-white font-bold py-5"><?php echo $heroHeaderTwo ?></<?php echo $heroHeaderSizeTwo ?>>
-                    <p class="text-white"><?php echo $heroText ?></p>
+                    <div class="text-white">
+                        <?php echo $heroText ?>
+                    </div>
                     <a href="<?php echo $heroButton ?>" class="btn">Get in contact today</a>
                 </div>
                 
